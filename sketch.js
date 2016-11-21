dd824df23d86b426cf3043f6df1832b5e7c22146
MMNMJ <longfactory13@hanmail.net>
2016-11-21 15:40:35 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,26 +7,26 @@ function setup() {
 function draw() {
   
 }
-}
 
-function draw() {
-  background(200,200,0);
-  rectMode(CENTER);
-  ellipseMode(CENTER);
+
+//function draw() {
+  //background(200,200,0);
+  //rectMode(CENTER);
+  //ellipseMode(CENTER);
   
   //Body
-  fill(0,0,255);
-  rect(240,310,50,120);
+  //fill(0,0,255);
+  //rect(240,310,50,120);
   
   //head
-  fill(100,140,20,200);
-  ellipse(240,219,60,60);
+  //fill(100,140,20,200);
+  //ellipse(240,219,60,60);
   
   //Legs
-  line(215,370,85,520);
-  line(265,370,400,520);
+  //line(215,370,85,520);
+  //line(265,370,400,520);
   
   //edit
-  rect(200,200,40,40);
+  //rect(200,200,40,40);
   
 }
